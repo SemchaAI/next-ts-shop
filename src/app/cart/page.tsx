b680964e7696c747a6ff776ef8a0caaaa6490df1
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-05 13:22:59 +0300

    + cart page update
    +animation

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,13 +1,18 @@
 'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import { useDeleteOneMutation } from '@/services/cartApi';
+import { useAppSelector } from '@/lib/hooks';
+
 import MainBtn from '@/components/buttons/MainBtn';
 import CloseIcon from '@/components/icons/CloseIcon';
 
 import css from './cartPage.module.scss';
-import { useDeleteOneMutation } from '@/services/cartApi';
-import { useAppSelector } from '@/lib/hooks';
-import Link from 'next/link';
-import Image from 'next/image';
+
 import { total } from '@/stores/cart.slice';
+import { IProduct } from '@/models/products';
 
 const url = process.env.NEXT_PUBLIC_BASE_URL_BD;
 
@@ -23,16 +28,27 @@ export default function CartPage() {
   const removeHandler = async (productId: string): Promise<void> => {
     deleteOne({ userId, productId });
   };
+
+  if (items.length === 0) {
+    return <div className={css.emptyCart}>Корзина пуста</div>;
+  }
+  if (!items) {
+    <div className={css.cartError}>Ошибка. Корзина не была загружена</div>;
+  }
+
   return (
     <section className={css.cart}>
       <div className="wrapper">
         <div className={css.cartContainer}>
           <h1 className={css.cartTitle}>Cart</h1>
-          {items.length > 0 ? (
-            <div className={css.cartBlock}>
-              <ul className={css.cartItems}>
-                {items.map((item) => (
-                  <li
+          <div className={css.cartBlock}>
+            <ul className={css.cartItems}>
+              <AnimatePresence>
+                {items.map((item: IProduct) => (
+                  <motion.li
+                    initial={{ opacity: 1 }}
+                    exit={{ opacity: [1, 0], translateX: [0, -2000] }}
+                    transition={{ duration: 0.6 }}
                     className={css.cartItem}
                     key={item._id}
                   >
@@ -61,7 +77,6 @@ export default function CartPage() {
                       version="outline"
                       icon={true}
                       onClick={() => removeHandler(item._id)}
-                      // className={css.cartItemBtn}
                     >
                       <CloseIcon
                         width={24}
@@ -69,34 +84,28 @@ export default function CartPage() {
                         className={css.cartItemBtnIcon}
                       />
                     </MainBtn>
-                  </li>
+                  </motion.li>
                 ))}
-              </ul>
-              <div className={css.cartOrder}>
-                <h3 className={css.cartTotalTitle}>Итого</h3>
-                <div className={css.cartTotal}>{totalPrice} MDL</div>
-                <div className={css.cartDelivery}>
-                  Доставка <span>бесплатно</span>
-                </div>
-                <p className={css.cartPolicy}>
-                  <span className={css.asterisk}>*</span>Покупая у нас товары вы
-                  соглашаетесь с политикой конфиденциальности
-                </p>
-                <Link
-                  className={css.cartLink}
-                  href="/order"
-                >
-                  Order Now (temporal to home)
-                </Link>
+              </AnimatePresence>
+            </ul>
+            <div className={css.cartOrder}>
+              <h3 className={css.cartTotalTitle}>Итого</h3>
+              <div className={css.cartTotal}>{totalPrice} MDL</div>
+              <div className={css.cartDelivery}>
+                Доставка <span>бесплатно</span>
               </div>
+              <p className={css.cartPolicy}>
+                <span className={css.asterisk}>*</span>Покупая у нас товары вы
+                соглашаетесь с политикой конфиденциальности
+              </p>
+              <Link
+                className={css.cartLink}
+                href="/"
+              >
+                Order Now (temporal to home)
+              </Link>
             </div>
-          ) : items.length === 0 ? (
-            <div className={css.emptyCart}>Корзина пуста</div>
-          ) : (
-            <div className={css.cartError}>
-              Ошибка. Корзина не была загружена
-            </div>
-          )}
+          </div>
         </div>
       </div>
     </section>
