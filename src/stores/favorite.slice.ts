cb200ef591d14b3f2ca0a177858cc0f587d110ed
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-05 13:33:21 +0300

    +sync favor with localstorage
    isnt best way

--- src/stores/favorite.slice.ts
+++ src/stores/favorite.slice.ts
@@ -1,5 +1,6 @@
 import { IProduct } from '@/models/products';
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+// import { cookies } from 'next/headers';
 
 interface IFavoriteSlice {
   items: IProduct[];
@@ -8,21 +9,51 @@ const initialState: IFavoriteSlice = {
   items: [],
 };
 
+// if (typeof window !== 'undefined') {
+//   const favorStore = localStorage.getItem('favorite');
+//   // const cookieStore = cookies().get('favorite');
+//   // if (cookieStore !== undefined) {
+//   //   initialState.items = JSON.parse(cookieStore.value);
+//   // }
+//   if (favorStore !== null) {
+//     initialState.items = JSON.parse(favorStore);
+//   }
+// }
+
+// #######################################
+// I DOESNT FOUND BEST WAY FOR SYNC FAVORITE
+// ONLY TO STORE IT ON SERVER
+// NOW I USE LOCALSTORAGE BECAUSE DONT WANT TO CHANGE BACKEND
+// I USE IT IN 3 MORE PROJECTS
+// NOW BECAUSE OF SYNC BETWEEN CLIENT AND SERVER
+// FAVOR PANEL  RERENDER TWICE (WITH INIT STATE [] AND AFTER IT CHANGES)
+// COOKIES DOESNT WANT TO WORK OUTSIDE PAGE.TSX COMPONENT
+
 export const favoriteSlice = createSlice({
   name: 'favorite',
   initialState,
 
   reducers: {
+    initFavorite: (state) => {
+      // console.log(document.cookie);
+      const favoriteStore = localStorage.getItem('favorite');
+      if (favoriteStore) {
+        state.items = JSON.parse(favoriteStore);
+      }
+    },
     addFavorite: (state, action: PayloadAction<IProduct>) => {
       console.log('add');
-      // state.items = [...state.items, action.payload];
       state.items.push(action.payload);
+      document.cookie = 'favorite=' + JSON.stringify(state.items);
+      // localStorage.setItem('favorite', JSON.stringify(state.items));
     },
     removeFavorite: (state, action: PayloadAction<IProduct>) => {
       console.log('remove');
       state.items = state.items.filter(
         (item) => item._id !== action.payload._id
       );
+      document.cookie = 'favorite=' + JSON.stringify(state.items);
+      // localStorage.setItem('favorite', JSON.stringify(state.items));
     },
     favoriteHandler: (state, action: PayloadAction<IProduct>) => {
       console.log('payload', action.payload);
@@ -46,5 +77,10 @@ export const isFavorite = (state: IFavoriteSlice, product: IProduct | null) => {
 // export const { getUser, getIsAuth, getIsLoading } = userSlice.selectors;
 
 // actions
-export const { reset, addFavorite, removeFavorite, favoriteHandler } =
-  favoriteSlice.actions;
+export const {
+  reset,
+  initFavorite,
+  addFavorite,
+  removeFavorite,
+  favoriteHandler,
+} = favoriteSlice.actions;
