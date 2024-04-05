a024d2ae71e5b48f9ed754db09a9f77f02302566
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-05 13:25:28 +0300

    + Home page metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,18 @@ import SearchInput from '@/components/inputs/SearchInput';
 import css from './homePage.module.scss';
 import ProductList from '@/components/products/ProductList/ProductList';
 import SidebarTypes from '@/components/sidebar/SidebarTypes';
+import HomeBanner from '@/components/sliders/homeBanner/HomeBanner';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Next ts shop | Home',
+  description: 'Micro frontend example of shop on next 14',
+};
+
 export default function Home() {
   return (
     <section className={css.products}>
       <div className="wrapper">
+        <HomeBanner />
         <div className={css.productsContainer}>
           <h1 className={css.productsTitle}>Все товары</h1>
           <div className={css.productsBlock}>
