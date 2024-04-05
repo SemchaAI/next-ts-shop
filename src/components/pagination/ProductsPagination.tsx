eef2efa8010fb19fe0700a01985f3d68b3fcce20
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-05 13:26:54 +0300

    + pagination logic
    now pages can be infinite and paginations will look nice

--- src/components/pagination/ProductsPagination.tsx
+++ src/components/pagination/ProductsPagination.tsx
@@ -3,10 +3,49 @@ import css from './productsPagination.module.scss';
 import ChevronRight from '../icons/ChevronRight';
 import { setPage } from '@/stores/product.slice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+
+enum EPageVariant {
+  DEFAULT = 'default',
+  FIRST = 'first',
+  MIDDLE = 'middle',
+  LAST = 'last',
+}
 export default function ProductsPagination() {
   const dispatch = useAppDispatch();
   const { page, total } = useAppSelector((state) => state.product);
 
+  const pages = [];
+  let pageVariant: EPageVariant = EPageVariant.DEFAULT;
+
+  // shit logic for pagination,
+  //mb version need to be send from backend
+  if (page > 4 && page < total - 2) {
+    console.log('MIDDLE');
+    pageVariant = EPageVariant.MIDDLE;
+    for (let i = page - 3; i < page + 2; i++) {
+      pages.push(i);
+    }
+    console.log('pages', pages);
+  } else if (page > 4 && page >= total - 2) {
+    console.log('LAST');
+    pageVariant = EPageVariant.LAST;
+    for (let i = page - 4; i < total - 1; i++) {
+      pages.push(i);
+    }
+  } else if (total < 8) {
+    console.log('DEFAULT');
+    pageVariant = EPageVariant.DEFAULT;
+    for (let i = 1; i < total - 1; i++) {
+      pageVariant = EPageVariant.DEFAULT;
+      pages.push(i);
+    }
+  } else {
+    console.log('FIRST');
+    for (let i = 1; i < 5; i++) {
+      pageVariant = EPageVariant.FIRST;
+      pages.push(i);
+    }
+  }
   const toPage = (i: number) => {
     if (page <= total && page > 0) {
       dispatch(setPage(i));
@@ -32,20 +71,47 @@ export default function ProductsPagination() {
         <ChevronLeft />
       </button>
       <ul className={css.paginationList}>
-        {[...Array(total)].map((_, i) => (
-          <li key={i}>
+        <li>
+          <button
+            onClick={() => toPage(1)}
+            className={`${css.paginationListBtn} ${
+              page === 1 ? css.active : ''
+            }`}
+          >
+            1
+          </button>
+        </li>
+        {(pageVariant === EPageVariant.MIDDLE ||
+          pageVariant === EPageVariant.LAST) && (
+          <div style={{ margin: '0 5px' }}>...</div>
+        )}
+        {pages.map((item, i) => (
+          <li key={item}>
             <button
-              onClick={() => toPage(i + 1)}
+              onClick={() => toPage(item + 1)}
               className={`${css.paginationListBtn} ${
-                page === i + 1 ? css.active : ''
+                page === item + 1 ? css.active : ''
               }`}
             >
-              {i + 1}
+              {item + 1}
             </button>
           </li>
         ))}
+        {(pageVariant === EPageVariant.MIDDLE ||
+          pageVariant === EPageVariant.FIRST) && (
+          <div style={{ margin: '0 5px' }}>...</div>
+        )}
+        <li>
+          <button
+            onClick={() => toPage(total)}
+            className={`${css.paginationListBtn} ${
+              page === total ? css.active : ''
+            }`}
+          >
+            {total}
+          </button>
+        </li>
       </ul>
-      {/* @click.prevent="nextPage" */}
       <button
         onClick={() => nextPage()}
         className={css.paginationListBtn}
