2832329c6bd75d7c959769ef727545693e73f31c
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-06 19:02:43 +0300

    +test comm with error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import LoginForm from '@/components/forms/loginForm/LoginForm';
 import css from './loginPage.module.scss';
 export default function Login() {
+  //  test error boundary by nextjs
+  // throw new Error('test error');
   return (
     <section className={css.login}>
       <div className="wrapper">
