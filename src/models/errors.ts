080d9c22ced5eb1af736061d427a01b65aaed76f
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-08 13:02:53 +0300

    + error types

---
+++ src/models/errors.ts
@@ -0,0 +1,22 @@
+export interface IError {
+  message: string;
+  critical: boolean;
+  id: number;
+}
+
+export interface IErrors {
+  errors: IError[];
+}
+export interface IRTKError {
+  status: number;
+  data: {
+    message: string;
+    errors: [];
+  };
+}
+
+export interface IErrorState extends IErrors {
+  lastAI: number;
+  timeout: number;
+}
+export type TError = Omit<IError, 'id'>;
