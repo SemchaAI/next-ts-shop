5163f6aacd6426e3d65d4cebd30b1ad8a9f04149
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-09 11:19:17 +0300

    +animation for favorite items

--- src/components/widgets/favoritePanel/FavoritePanel.tsx
+++ src/components/widgets/favoritePanel/FavoritePanel.tsx
@@ -8,9 +8,9 @@ import CloseIcon from '@/components/icons/CloseIcon';
 
 import type { IProduct } from '@/models/products';
 
-import { initFavorite, removeFavorite } from '@/stores/favorite.slice';
+import { removeFavorite } from '@/stores/favorite.slice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
-import { useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const url = process.env.NEXT_PUBLIC_BASE_URL_BD;
 
@@ -32,50 +32,56 @@ export default function FavoritePanel() {
   return (
     <div className={css.favoriteBlock}>
       <ul className={css.favoriteItems}>
-        {favorItems.map((item) => (
-          <li
-            className={css.favoriteItem}
-            key={item._id}
-          >
-            <div className={css.favoriteItemContainer}>
-              <Link
-                className={css.productLink}
-                href={`product/${item._id}`}
-              >
-                <Image
-                  priority={true}
-                  width="200"
-                  height="200"
-                  className={css.favoriteItemImg}
-                  src={url + '/' + item.img}
-                  alt={item.title}
-                />
-              </Link>
-              <div className={css.favoriteItemInfo}>
-                <div className={css.favoriteItemTitle}>{item.title}</div>
-                <div className={css.favoriteItemDescription}>
-                  {item.description}
-                </div>
-                <div className={css.favoriteControls}>
-                  <div className={css.favoriteItemPrice}>
-                    <span>Цена:</span>
-                    {item.price}
-                    <b>MDL</b>
+        <AnimatePresence mode="popLayout">
+          {favorItems.map((item) => (
+            <motion.li
+              layout
+              initial={{ opacity: 1 }}
+              exit={{ opacity: 0, translateX: [0, -2000] }}
+              transition={{ duration: 0.6 }}
+              className={css.favoriteItem}
+              key={item._id}
+            >
+              <div className={css.favoriteItemContainer}>
+                <Link
+                  className={css.productLink}
+                  href={`product/${item._id}`}
+                >
+                  <Image
+                    priority={true}
+                    width="200"
+                    height="200"
+                    className={css.favoriteItemImg}
+                    src={url + '/' + item.img}
+                    alt={item.title}
+                  />
+                </Link>
+                <div className={css.favoriteItemInfo}>
+                  <div className={css.favoriteItemTitle}>{item.title}</div>
+                  <div className={css.favoriteItemDescription}>
+                    {item.description}
                   </div>
-                  <MainBtn
-                    onClick={() => removeHandler(item)}
-                    icon={true}
-                  >
-                    <CloseIcon
-                      width={24}
-                      height={24}
-                    />
-                  </MainBtn>
+                  <div className={css.favoriteControls}>
+                    <div className={css.favoriteItemPrice}>
+                      <span>Цена:</span>
+                      {item.price}
+                      <b>MDL</b>
+                    </div>
+                    <MainBtn
+                      onClick={() => removeHandler(item)}
+                      icon={true}
+                    >
+                      <CloseIcon
+                        width={24}
+                        height={24}
+                      />
+                    </MainBtn>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </motion.li>
+          ))}
+        </AnimatePresence>
       </ul>
     </div>
   );
