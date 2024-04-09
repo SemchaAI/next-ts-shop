96f9d15b261d94ed92008372371ad9260a6e647e
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-09 11:19:35 +0300

    +protect admin route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { appStore } from './lib/features/storesInit/appStore';
 
 export const config = {
   matcher: ['/admin/:path*'],
 };
 
-export function middleware(request: NextRequest) {
-  const role = appStore.getState().user.user.role;
-  console.log('MIDDLEWARE-----------------', request.url, role);
-  if (role !== 'ADMIN') {
+export async function middleware(request: NextRequest) {
+  const role = request.cookies.get('role');
+
+  console.log('MIDDLEWARE-----------------', request.url, role?.value);
+
+  if (role?.value !== 'ADMIN') {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 }
