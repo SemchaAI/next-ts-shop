7d7e90c096e22a8d6a09fdde90478a69a57ae135
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-10 13:26:11 +0300

    +loading state for individual product

---
+++ src/app/product/[productId]/loading.tsx
@@ -0,0 +1,6 @@
+import ProductInfoSkeleton from '@/components/products/ProductInfo/ProductInfoSkeleton';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <ProductInfoSkeleton variant="loading" />;
+}
