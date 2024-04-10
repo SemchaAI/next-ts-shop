18669cd359f92c05f21f215572410718d010e753
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-10 13:30:08 +0300

    +Loading/Error Skeleton

--- src/components/products/ProductInfo/ProductInfoSkeleton.tsx
+++ src/components/products/ProductInfo/ProductInfoSkeleton.tsx
@@ -1,3 +1,30 @@
-export default function ProductInfoSkeleton() {
-  return <div>Loading...</div>;
+import LoadingSpinner from '@/components/indicators/LoadingSpinner';
+
+interface IProps {
+  variant: 'error' | 'loading';
+  errorMessage?: string;
+}
+
+export default function ProductInfoSkeleton({ variant, errorMessage }: IProps) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        flexGrow: 1,
+      }}
+    >
+      {variant === 'loading' && (
+        <>
+          <div>Loading... Please wait</div>
+          <div style={{ width: '80px' }}>
+            <LoadingSpinner />
+          </div>
+        </>
+      )}
+      {variant === 'error' && <div>Error - {errorMessage}</div>}
+    </div>
+  );
 }
