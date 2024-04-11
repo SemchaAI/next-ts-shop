cc40c618d4c0ec22f2397350ded8fab2ed96f321
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-04-11 19:11:35 +0300

    +localeSwitcher
    serv component for useLocale hook

--- src/components/lang/LocaleSwitcher.tsx
+++ src/components/lang/LocaleSwitcher.tsx
@@ -1,22 +1,35 @@
 import { useLocale, useTranslations } from 'next-intl';
 import { locales } from '@/i18n';
-import LocaleSwitcherSelect from './LocaleSwitcherSelect';
+import LocaleSwitcherSelect from './LocaleSwitcherSelect(deprecated)';
+import CustomSelect from './CustomSelect';
 
 export default function LocaleSwitcher() {
   const t = useTranslations('LocaleSwitcher');
   const locale = useLocale();
 
+  let localeArray = locales.map((cur) => ({
+    value: cur,
+    label: t('locale', { locale: cur }),
+  }));
+  console.log('localeArray', localeArray);
+  const ChangeHandler = (value: string) => {
+    console.log('value', value);
+  };
   return (
-    <LocaleSwitcherSelect defaultValue={locale}>
-      {locales.map((cur) => (
-        <option
-          key={cur}
-          value={cur}
-          disabled={cur === locale}
-        >
-          {t('locale', { locale: cur })}
-        </option>
-      ))}
-    </LocaleSwitcherSelect>
+    // <LocaleSwitcherSelect defaultValue={locale}>
+    //   {locales.map((cur) => (
+    //     <option
+    //       key={cur}
+    //       value={cur}
+    //       disabled={cur === locale}
+    //     >
+    //       {t('locale', { locale: cur })}
+    //     </option>
+    //   ))}
+    // </LocaleSwitcherSelect>
+    <CustomSelect
+      defaultValue={t('locale', { locale: locale })}
+      options={localeArray}
+    />
   );
 }
