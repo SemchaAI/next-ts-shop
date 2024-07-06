5137d82410d56343d557406a5e49cdc8636141cc
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-07-06 19:42:17 +0300

    +back to default

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import type { IUser } from './models/user';
+// import type { IUser } from './models/user';
 // import createMiddleware from 'next-intl/middleware';
 // import { locales, localePrefix, pathnames } from './navigation';
 
@@ -21,16 +21,11 @@ export const config = {
 
 export async function middleware(request: NextRequest) {
   //tmp decision
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_BD}/api/user`);
-  const { user } = (await data.json()) as { user: IUser };
-  // normal decision  when you have single domain
-  // const role = request.cookies.get('role');
-  // console.log('MIDDLEWARE';
-  // if (role.?value !== 'ADMIN') {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
-
-  if (user.role !== 'ADMIN') {
+  // const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_BD}/api/user`);
+  // const { user } = (await data.json()) as { user: IUser };
+  const role = request.cookies.get('role');
+  console.log('MIDDLEWARE', role, request.cookies.getAll());
+  if (role?.value !== 'ADMIN') {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 }
